ee41367688145e7781ea84473ecb6b48363a735f
Ashwani kr <a61ashwanikumar@gmail.com>
2021-07-17 00:45:48 +0530

    pkk

--- scripts/app.js
+++ scripts/app.js
@@ -66,3 +66,65 @@ function step(timestamp) {
 }
 
 requestId = window.requestAnimationFrame(step);
+
+
+
+
+
+
+
+// credit: http://www.javascriptkit.com/javatutors/touchevents2.shtml
+function swipedetect(el, callback) {
+
+  var imageContainer = el,
+    swipedir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 100, //required min distance traveled to be considered swipe
+    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 200, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime,
+    handleswipe = callback || function (swipedir) { }
+
+  imageContainer.addEventListener('touchstart', function (e) {
+    var touchobj = e.changedTouches[0]
+    swipedir = 'none'
+    dist = 0
+    startX = touchobj.pageX
+    startY = touchobj.pageY
+    startTime = new Date().getTime() // record time when finger first makes contact with surface
+    e.preventDefault()
+  }, false)
+
+  imageContainer.addEventListener('touchmove', function (e) {
+    e.preventDefault() // prevent scrolling when inside DIV
+  }, false)
+
+  imageContainer.addEventListener('touchend', function (e) {
+    var touchobj = e.changedTouches[0]
+    distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+    distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+    elapsedTime = new Date().getTime() - startTime // get time elapsed
+    if (elapsedTime <= allowedTime) { // first condition for awipe met
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+        (distX < 0) ? arrowClicked('left') : arrowClicked('right'); // if dist traveled is negative, it indicates left swipe
+      }
+      // else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+      //   swipedir = (distY < 0) ? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+      // }
+    }
+    handleswipe(swipedir)
+    e.preventDefault()
+  }, false)
+}
+
+//USAGE:
+
+var el = document.getElementById('swipezone');
+swipedetect(el, function (swipedir) {
+  // swipedir contains either "none", "left", "right", "top", or "down"
+  el.innerHTML = 'Swiped <span style="color:yellow;margin: 0 5px;">' + swipedir + '</span>';
+});
